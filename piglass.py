be21f97be0a3981556c3c9107296bdbfa3874ac2
Matt <matt.desmarais78@gmail.com>
2017-08-04 21:13:48 -0400

    added gui toggle to button 18

--- piglass.py
+++ piglass.py
@@ -7,62 +7,10 @@ import datetime
 import cv2
 import numpy as np
 
-globals = {
-    'zoom_step'     : 0.03,
-
-    'zoom_xy_min'   : 0.0,
-    'zoom_xy'       : 0.0,
-    'zoom_xy_max'   : 0.4,
-
-    'zoom_wh_min'   : 1.0,
-    'zoom_wh'       : 1.0,
-    'zoom_wh_max'   : 0.2
-}
-
-camera = picamera.PiCamera()
-
-# initial config for gpio ports
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-
-pin_17 = False
-pin_18 = False
-pin_26 = False
-
-alphaValue = 64
-
 height = 600
 width = 800
-
-if (width%32) > 0 or (height%16) > 0:
-    print "Rounding down set resolution to match camera block size:"
-    width = width-(width%32)
-    height = height-(height%16)
-    print "New resolution: " + str(width) + "x" + str(height)
-
-colors = {
-        'white': (255,255,255),
-        'red': (255,0,0),
-        'green': (0,255,0),
-        'blue': (0,0,255),
-        'yellow': (255,255,0),
-        }
-
-def colormap(col):
-    return colors.get(col, (255,255,255))
-
-col = colormap('red')
-font = cv2.FONT_HERSHEY_PLAIN
-#height = 600
-#width = 800
-
-gui = np.zeros((height, width, 3), dtype=np.uint8)
-gui1 = 'PiGlass'
-gui2 = 'Version 0.1 alpha'
-gui3 = 'button  = take pic'
-
-#def colormap(col):
-#    return colors.get(col, (255,255,255))
+alphaValue = 64
+o = None
 
 def initialize_camera():
     camera.resolution = (width, height)
@@ -81,47 +29,97 @@ def initialize_camera():
     camera.rotation = -90
     camera.hflip = False
     camera.vflip = False
-    #set zoom to (almost) line up with your field of vision
-    zoom_in()
-    zoom_in()
-    zoom_in()
-    zoom_in()
-    zoom_in()
-    zoom_in()
-    zoom_in()
-    # (x, y, w, h)
-    #set_min_zoom()
     camera.start_preview()
     print "Camera is configured and outputting video..."
 
+camera = picamera.PiCamera()
+if (width%32) > 0 or (height%16) > 0:
+    print "Rounding down set resolution to match camera block size:"
+    width = width-(width%32)
+    height = height-(height%16)
+    print "New resolution: " + str(width) + "x" + str(height)
+#initialize_camera()
+
+ovl = np.zeros((height, width, 3), dtype=np.uint8)
+
+globalz = {
+    'zoom_step'     : 0.03,
+
+    'zoom_xy_min'   : 0.0,
+    'zoom_xy'       : 0.0,
+    'zoom_xy_max'   : 0.4,
+
+    'zoom_wh_min'   : 1.0,
+    'zoom_wh'       : 1.0,
+    'zoom_wh_max'   : 0.2,
+#    'o'             : camera.add_overlay(np.getbuffer(ovl), layer=3, alpha=alphaValue)
+
+}
+
+#o = camera.add_overlay(np.getbuffer(ovl), layer=3, alpha=alphaValue)
+
 def update_zoom():
-    #print "Setting camera to (%s, %s, %s, %s)" % (globals['zoom_xy'], globals['zoom_xy'], globals['zoom_wh'], globals['zoom_wh'])
-    camera.zoom = (globals['zoom_xy'], globals['zoom_xy'], globals['zoom_wh'], globals['zoom_wh'])
+    #print "Setting camera to (%s, %s, %s, %s)" % (globals['zoom_xy'], globals[$
+    camera.zoom = (globalz['zoom_xy'], globalz['zoom_xy'], globalz['zoom_wh'], globalz['zoom_wh'])
     print "Camera at (x, y, w, h) = ", camera.zoom
 
 def set_min_zoom():
-    globals['zoom_xy'] = globals['zoom_xy_min']
-    globals['zoom_wh'] = globals['zoom_wh_min']
+    globalz['zoom_xy'] = globalz['zoom_xy_min']
+    globalz['zoom_wh'] = globalz['zoom_wh_min']
 
 def set_max_zoom():
-    globals['zoom_xy'] = globals['zoom_xy_max']
-    globals['zoom_wh'] = globals['zoom_wh_max']
+    globalz['zoom_xy'] = globalz['zoom_xy_max']
+    globalz['zoom_wh'] = globalz['zoom_wh_max']
 
 def zoom_out():
-    if globals['zoom_xy'] - globals['zoom_step'] < globals['zoom_xy_min']:
+    if globalz['zoom_xy'] - globalz['zoom_step'] < globalz['zoom_xy_min']:
         set_min_zoom()
     else:
-        globals['zoom_xy'] -= globals['zoom_step']
-        globals['zoom_wh'] += (globals['zoom_step'] * 2)
+        globalz['zoom_xy'] -= globalz['zoom_step']
+        globalz['zoom_wh'] += (globalz['zoom_step'] * 2)
     update_zoom()
 
 def zoom_in():
-    if globals['zoom_xy'] + globals['zoom_step'] > globals['zoom_xy_max']:
+    if globalz['zoom_xy'] + globalz['zoom_step'] > globalz['zoom_xy_max']:
         set_max_zoom()
     else:
-        globals['zoom_xy'] += globals['zoom_step']
-        globals['zoom_wh'] -= (globals['zoom_step'] * 2)
+        globalz['zoom_xy'] += globalz['zoom_step']
+        globalz['zoom_wh'] -= (globalz['zoom_step'] * 2)
     update_zoom()
+
+ovl = np.zeros((height, width, 3), dtype=np.uint8)
+alphaValue = 64
+
+# initial config for gpio ports
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+pin_17 = False
+pin_18 = False
+pin_26 = False
+
+colors = {
+        'white': (255,255,255),
+        'red': (255,0,0),
+        'green': (0,255,0),
+        'blue': (0,0,255),
+        'yellow': (255,255,0),
+        }
+
+def colormap(col):
+    return colors.get(col, (255,255,255))
+
+col = colormap('red')
+font = cv2.FONT_HERSHEY_PLAIN
+
+guivisible = 1
+togsw = 1
+guiOn = 1
+gui = np.zeros((height, width, 3), dtype=np.uint8)
+gui1 = 'PiGlass'
+gui2 = 'Version 0.1 alpha'
+gui3 = 'button  = take pic'
+
 
 def get_file_name():  # new
     return datetime.datetime.now().strftime("%Y-%m-%d_%H.%M.%S.jpg")
@@ -131,6 +129,58 @@ def creategui(target):
     cv2.putText(target, gui2, (10,height-108), font, 2, col, 2)
     cv2.putText(target, gui3, (10,height-78), font, 2, col, 2)
     #camera.add_overlay(np.getbuffer(target), layer=3, alpha=alphaValue)
+    return
+
+def patternswitch(target,guitoggle):
+    global o, alphaValue
+    # first remove existing overlay:
+    # cycle through possible patterns:
+    if guitoggle == 1:
+	    creategui(gui)
+    o = camera.add_overlay(np.getbuffer(target), layer=3, alpha=alphaValue)
+    return
+
+def togglepattern(channel):
+    global togsw,o,ovl,gui,alphaValue
+    # if overlay is inactive, ignore button:
+    if togsw == 0:
+        print "Pattern button pressed, but ignored --- Crosshair not visible."
+    # if overlay is active, drop it, change pattern, then show it again
+    else:
+        #curpat += 1
+        #print "Set new pattern: " + str(curpat) 
+        #if curpat > patterns.maxpat:     # this number must be adjusted to number of available patterns!
+        #    curpat = 1
+        if guivisible == 0:
+            # reinitialize array:
+            ovl = np.zeros((height, width, 3), dtype=np.uint8)
+            patternswitch(ovl,0)
+            #if o in globalz():
+            #    camera.remove_overlay(o)
+            #o = camera.add_overlay(np.getbuffer(ovl), layer=3, alpha=alphaValue)
+        else:
+            # reinitialize array
+            gui = np.zeros((height, width, 3), dtype=np.uint8)
+            creategui(gui)
+            patternswitch(gui,1)
+            if 'o' in globalz():
+                camera.remove_overlay(o)
+            o = camera.add_overlay(np.getbuffer(gui), layer=3, alpha=alphaValue)
+    return
+
+def toggleonoff(channel):
+    global togsw,o,alphaValue
+    if togsw == 1:
+        print "Toggle Crosshair OFF"
+        camera.remove_overlay(o)
+        togsw = 0
+    else:
+        print "Toggle Crosshair ON"
+        if guivisible == 0:
+            o = camera.add_overlay(np.getbuffer(ovl), layer=3, alpha=alphaValue)
+        else:
+            o = camera.add_overlay(np.getbuffer(gui), layer=3, alpha=alphaValue)
+        togsw = 1
     return
 
 def button_pressed_26(pin):
@@ -156,9 +206,16 @@ def button_pressed_17(pin):
     #    time.sleep(0.2)
 
 def button_pressed_18(pin):
-    global pin_18
+    global pin_18, gui, ovl, guiOn
     print "pin:", pin
-    zoom_out()
+    #zoom_out()
+    if guiOn == 0:
+        patternswitch(ovl, guiOn)
+        guiOn = 1
+    if guiOn == 1:
+        patternswitch(ovl, guiOn)
+	guiOn = 0
+         
     #print "pin_18", pin_18
     #while pin_17 == True:
     #    print('GPIO #18 button pressed')
@@ -178,18 +235,35 @@ def configure_button_listeners():
     # GPIO.FALLING
     # GPIO.BOTH
     GPIO.add_event_detect(17, GPIO.FALLING, callback=button_pressed_17, bouncetime=200)
-    GPIO.add_event_detect(18, GPIO.FALLING, callback=button_pressed_18, bouncetime=200)
+    GPIO.add_event_detect(18, GPIO.FALLING, callback=toggleonoff, bouncetime=750)
     GPIO.add_event_detect(26, GPIO.FALLING, callback=button_pressed_26, bouncetime=200)
 
     print "Button Listeners are configured and listening..."
 
 def main():
-    configure_button_listeners()
-    initialize_camera()
-    gui = np.zeros((height, width, 3), dtype=np.uint8)
-    creategui(gui)
-    camera.add_overlay(np.getbuffer(gui), layer=3, alpha=alphaValue)
-    raw_input() # Run the loop function to keep script running
+    try:
+        configure_button_listeners()
+        initialize_camera()
+        zoom_in()
+        zoom_in()
+        zoom_in()
+        zoom_in()
+        zoom_in()
+        zoom_in()
+        zoom_in()
+        gui = np.zeros((height, width, 3), dtype=np.uint8)
+        patternswitch(gui,1)
+        time.sleep(10)
+        guivisible = 1
+        # cycle through possible patterns:
+        patternswitch(ovl,0)
+        while True:
+            #print('running')
+	        x = 1
+    finally:
+        camera.close()               # clean up camera
+        GPIO.cleanup()               # clean up GPIO
+
 
 if __name__ == "__main__":
     main()
